141056a0fe82ccc077e87aa197ac8b9c5419f934
mifsudm <mifsudm@unimelb.edu.au>
2018-01-15 09:06:13 +1100

    ~Auto: Minor Code Updates - 252s-dev

--- index.php
+++ index.php
@@ -4,11 +4,15 @@
  * @link http://www.tropotek.com/
  * @license Copyright 2007 Michael Mifsud
  */
+
 include(dirname(__FILE__) . '/_prepend.php');
 
-$request = \App\Factory::getRequest();
-$kernel = \App\Factory::getFrontController();
+try {
+    $request = \App\Factory::getRequest();
+    $kernel = \App\Factory::getFrontController();
 
-$response = $kernel->handle($request)->send();
-$kernel->terminate($request, $response);
-
+    $response = $kernel->handle($request)->send();
+    $kernel->terminate($request, $response);
+} catch (Exception $e) {
+    \Tk\Log::error($e->__toString());
+}

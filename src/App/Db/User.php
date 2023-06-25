525a3aeed2866e814b86864ca84ab62d795d6d44
tropotek <info@tropotek.com>
2023-06-25 16:22:19 +1000

    ~Auto: Commit

--- src/App/Db/User.php
+++ src/App/Db/User.php
@@ -5,20 +5,9 @@ namespace App\Db;
 class User extends \Bs\Db\User
 {
     /**
-     * permission values
-	 * permissions are bit masks that can include on or more bits
-	 * requests for permission are ANDed with the user's permissions
-	 * if the result is non-zero the user has permission.
-     *
-     * high-level permissions for specific roles
+     * Wiki user permission values
      */
-	const PERM_ADMIN              = 0x00000001; // All permissions
-	const PERM_SYSADMIN           = 0x00000002; // Change system settings
-	const PERM_MANAGE_STAFF       = 0x00000004; // Manage staff users
-    // NOTE: Users should only have read access in the WIKI at this time...
-    const PERM_MANAGE_USER        = 0x00000008; // Manage base users
     const PERM_EDITOR             = 0x00000010; // Ability to edit/audit all site pages except private pages
-	//                            0x00000010; // available
 
 	/**
      * permission groups and descriptions
@@ -30,16 +19,6 @@ class User extends \Bs\Db\User
         self::PERM_MANAGE_USER    => "Manage Users",
         self::PERM_EDITOR         => "Content Editor",
     ];
-
-    /**
-     * Site staff user
-     */
-    const TYPE_STAFF = 'staff';
-
-    /**
-     * Base logged-in user type (Access to user pages)
-     */
-    const TYPE_USER = 'user';
 
 
 }

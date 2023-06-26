e23d60deba70a370fc0d1a185e44e4551d3bfdba
tropotek <info@tropotek.com>
2023-06-26 16:11:42 +1000

    ~Auto: Commit

--- html/assets/app.js
+++ html/assets/app.js
@@ -52,25 +52,30 @@ let app = function () {
 
     // Scroll-to-top button
     let mybutton = document.getElementById("btn-back-to-top");
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function () {
-      scrollFunction();
-    };
-    function scrollFunction() {
-      if (
-        document.body.scrollTop > 20 ||
-        document.documentElement.scrollTop > 20
-      ) {
-        mybutton.style.display = "block";
-      } else {
-        mybutton.style.display = "none";
+    if (mybutton) {
+      // When the user scrolls down 20px from the top of the document, show the button
+      window.onscroll = function () {
+        scrollFunction();
+      };
+
+      function scrollFunction() {
+        if (
+          document.body.scrollTop > 20 ||
+          document.documentElement.scrollTop > 20
+        ) {
+          mybutton.style.display = "block";
+        } else {
+          mybutton.style.display = "none";
+        }
       }
-    }
-    // When the user clicks on the button, scroll to the top of the document
-    mybutton.addEventListener("click", backToTop);
-    function backToTop() {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
+
+      // When the user clicks on the button, scroll to the top of the document
+      mybutton.addEventListener("click", backToTop);
+
+      function backToTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
     }
 
   };
@@ -142,7 +147,7 @@ let app = function () {
    */
   let initTinymce = function () {
     if (typeof tinymce === "undefined") {
-      console.warn('Plugin not loaded: jquery.tinymce');
+      vd('Plugin not loaded: jquery.tinymce');
       return;
     }
 

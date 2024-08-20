7bdf159332aac13b082f79bbf19ccb8eccf1407e
tropotek <info@tropotek.com>
2024-08-20 17:16:54 +1000

    ~Auto: Commit

--- src/config/sql/migrate/000102.sql
+++ src/config/sql/migrate/000102.sql
@@ -3,3 +3,5 @@
 -- --------------------------------------------
 
 ALTER TABLE user DROP COLUMN hash;
+ALTER TABLE user ALTER COLUMN username SET DEFAULT '';
+ALTER TABLE user ALTER COLUMN timezone SET DEFAULT '';

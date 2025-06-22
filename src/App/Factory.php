133d1c5c71e771087aadc4d1fd2ed19a020b4985
tropotek <info@tropotek.com>
2025-06-23 06:44:16 +1000

    ~Auto: Commit

--- src/App/Factory.php
+++ src/App/Factory.php
@@ -5,11 +5,14 @@ use App\Console\Cron;
 use App\Console\Test;
 use App\Console\TestData;
 use App\Console\WikiTest;
+use App\Db\User;
 use App\Dom\Modifier\CategoryList;
 use App\Dom\Modifier\SecretList;
 use App\Dom\Modifier\Secrets;
 use App\Dom\Modifier\WikiImg;
 use App\Dom\Modifier\WikiUrl;
+use Bs\Auth;
+use Bs\Db\UserInterface;
 use Bs\Registry;
 use Bs\Ui\Breadcrumbs;
 use Dom\Modifier;
@@ -65,7 +68,6 @@ class Factory extends \Bs\Factory
             $app = parent::getConsole();
             // Setup App Console Commands
             $app->add(new Cron());
-            $app->add(new \App\Console\CreateAdmin());
             if (Config::isDev()) {
                 $app->add(new WikiTest());
                 $app->add(new TestData());
@@ -73,5 +75,26 @@ class Factory extends \Bs\Factory
             }
         }
         return $this->get('console');
+    }
+
+    /**
+     *
+     */
+    public function createNewUser(string $username, string $email, string $password, int $perms = 0, string $type = ''): ?UserInterface
+    {
+        $user = new User();
+        $user->givenName = ucfirst($username);
+        $user->type = $type ?: User::TYPE_STAFF;
+        $user->country = 'AU';
+        $user->save();
+
+        $auth = Auth::create($user);
+        $auth->username = $username;
+        $auth->email = $email;
+        $auth->permissions = $perms;
+        $auth->password = Auth::hashPassword($password);
+        $auth->save();
+
+        return $user;
     }
 }
